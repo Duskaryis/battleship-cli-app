fed01dd328e516d320d226e61745da439ad8b568
Derek <derekpayne1991@hotmail.com>
2024-10-30 19:30:40 -0400

    Working on printing board

--- board.js
+++ board.js
@@ -1,28 +1,31 @@
 const readlineSync = require("readline-sync");
 
+
+
 function printBoard(board, debug) {
 	const displayBoard = {};
-
+	
 	for (let row = 0; row < board.length; row++) {
 		const rowLabel = String.fromCharCode(65 + row);
 		displayBoard[rowLabel] = [];
-
+		
 		for (let col = 0; col < board[row].length; col++) {
 			const cell = board[row][col];
 			if (debug) {
 				displayBoard[rowLabel].push(cell.type === "empty" ? "-" : cell.id);
-			} else {
-				if (cell.hit) {
-					displayBoard[rowLabel].push(cell.type === "empty" ? "❗" : "🟠");
 				} else {
-					displayBoard[rowLabel].push("-");
-				}
-			}
-		}
-	}
-	console.table(displayBoard);
-}
-
+					if (cell.hit) {
+						displayBoard[rowLabel].push(cell.type === "empty" ? "❗" : "🟠");
+						} else {
+							displayBoard[rowLabel].push("-");
+							}
+							}
+							}
+							}
+							console.table(displayBoard);
+							}
+							
+							
 function initializeBoard(rows, cols) {
 	const board = [];
 	for (let row = 0; row < rows; row++) {
@@ -33,6 +36,6 @@ function initializeBoard(rows, cols) {
 		board.push(boardRow);
 	}
 	return board;
-}
+} 
 
 module.exports = { printBoard, initializeBoard };
--- game.js
+++ game.js
@@ -3,26 +3,50 @@ const { printBoard, initializeBoard } = require("./board");
 const { computerPlacement, playerPlacement } = require("./shipPlacement");
 
 function greetUser() {
-	console.log("🚢  Welcome to BattleShip! 🚢");
-	if (readlineSync.keyInYN("Would you want to play?")) {
-		startGame();
-	} else {
-		console.log("Okay! Maybe another time then!");
-	}
+    console.log("🚢 Welcome to BattleShip! 🚢");
+    if( readlineSync.keyInYN("Would you want to play?")) {
+        setUpBoard();
+    }
+    else {
+        console.log("Okay! Maybe another time then!")
+    }
+}
+
+function setUpBoard() {
+    const boardSizeOptions = ["4X4", "5X5", "6X6"];
+    const boardChoice = readlineSync.keyInSelect(boardSizeOptions, "Choose a board size: ");
+    const boardSize = boardSizeOptions[boardChoice].split("x").map(Number);
+    startGame(boardSize);
+}
+
+function startGame() {
+    const playerBoard = initializeBoard(boardSize, board);
+    const computerBoard = initializeBoard(boardSize, board);
+    console.log("The Boards are set, PREPARE FOR BATTLE!");
+    printBoard(playerBoard, true);
+    playerTurn(playerBoard, computerBoard);
+}
+
+function playerTurn() {
+    let gameOver = false;
+    while(!gameOver) {
+        const playerGuess = readline.question("Make a guess! (eg. A1 or B2)");
+        const guessResult =  processGuess(playerGuess, computerBoard);
+        console.log(`Your guess: ${playerGuess} - ${guessResult ? "Hit!" : "Miss!" }`);
+
+        const computerGuess = generateComputerGuess(playerBoard);
+        const computerResult = processGuess(computerGuess, playerBoard);
+        console.log(`Computer's guess: ${computerGuess} - ${computerResult ? "Hit!" : "Miss!"}`);
+        
+        printBoard(playerBoard);
+        printBoard(computerBoard);
+
+        gameOver = checkWinCondition(playerBoard, computerBoard);
+    }
+    console.log("Game Over! Thanks for playing!")
 }
 
 greetUser();
-
-function startGame() {
-	const boardSizeOptions = ["4X4", "5X5", "6X6"];
-	const boardChoice = readlineSync.keyInSelect(
-		boardSizeOptions,
-		"Choose a board size: "
-	);
-	if (boardChoice === -1) {
-		console.log("Set up canceled. Exiting...");
-	}
-	const boardSize = boardSizeOptions[boardChoice].split("x").map(Number);
-	const board = initializeBoard(boardSize[0], boardSize[1]);
-	startGame(printBoard);
-}
+setUpBoard();
+startGame();
+playerTurn();
